497ad70f8fa12e2815b7b5bd21d134b2e90644a6
hgulese <hgulese02@gmail.com>
2022-12-22 20:13:39 +0300

    Add click listener added.

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 let ul = document.getElementById("task-list");
+let btnAdd = document.querySelector("#btnAddNewTask");
+let btnRemove = document.querySelector("#btnRemoveTask")
+let txtTaskName = document.querySelector("#txtTaskName");
 
 let taskList = [
     { "id": 1, "taskName": "Task 1" },
     { "id": 2, "taskName": "Task 2" },
     { "id": 3, "taskName": "Task 3" },
-    { "id": 4, "taskName": "Task 4" }
-]
+    { "id": 4, "taskName": "Task 4" },
+];
 
-for (let task of taskList) {
-    let li = `
-        <li class="task list-group-item">
-            <div class="form-check">
-                <input type="checkbox" id="${task.id}" class="form-check-input">
-                <label for="${task.id}" class="form-check-label">${task.taskName}</label>
-            </div>
-        </li>
-    `;
+displayTask();
+
+function displayTask() {
+    ul.innerHTML = "";
+
+    for (let task of taskList) {
+        let li = `
+            <li class="task list-group-item">
+                <div class="form-check">
+                    <input type="checkbox" id="${task.id}" class="form-check-input">
+                    <label for="${task.id}" class="form-check-label">${task.taskName}</label>
+                </div>
+            </li>
+        `;
 
 
-    ul.insertAdjacentHTML("beforeend", li);
+        ul.insertAdjacentHTML("beforeend", li);
+    }
 }
+
+btnAdd.addEventListener("click", (event) => {
+
+    if (txtTaskName.value != "") {
+        taskList.push({ "id": taskList.length + 1, "taskName": txtTaskName.value });
+        displayTask();
+
+        txtTaskName.value = "";
+    } else {
+        alert("Please! Enter a task name.")
+    }
+
+    event.preventDefault();
+});
